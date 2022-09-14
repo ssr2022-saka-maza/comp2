960f8f0323656ad074a95bdae043900bd4c878bd
H1rono <hronok66@gmail.com>
2022-09-14 16:11:26 +0900

    Add LowerBody

---
+++ include/ssr2/LowerBody.hpp
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <Arduino.h>
+#include "ssr2/Motor.hpp"
+#include "ssr2/Vector2D.hpp"
+#include "ssr2/MovingAverage.hpp"
+
+namespace ssr2 {
+    /**
+     * @brief 足回りを操作
+     */
+    class LowerBody {
+    private:
+        const Vector2D<float> _dir1, _dir2, _dir3;
+        Motor _motor1, _motor2, _motor3;
+        MovingAverage<float> _moving_average1, _moving_average2, _moving_average3;
+
+    public:
+        LowerBody() = delete;
+        LowerBody(const LowerBody &) = delete;
+        LowerBody & operator=(const LowerBody &) = delete;
+        LowerBody(LowerBody&&) = delete;
+        LowerBody & operator=(LowerBody&&) = delete;
+        ~LowerBody() = default;
+
+        explicit LowerBody(
+            uint8_t dir1, uint8_t pwm1,
+            uint8_t dir2, uint8_t pwm2,
+            uint8_t dir3, uint8_t pwm3,
+            uint16_t movAveCapacity = 64
+        );
+
+        /**
+         * @note
+         * # 各軸の設定など
+         * x軸: 重心から正面の方向
+         * y軸: 重心からx軸の正方向を向いて垂直に左方向
+         *
+         * # モーター配置の詳細
+         *           正面
+         * +----------^----------+
+         * |        --1--        |
+         * |                     |
+         * |          x          |
+         * |         y*          |
+         * |  \               /  |
+         * |   2             3   |
+         * |    \           /    |
+         * +---------------------+
+         * 配置は上のAsciiArtの通り。真ん中の*が中心、軸はxyって書いてあるやつが各軸の正方向。各タイヤは反時計回りを正転方向で
+         *
+         * # 各変数名の定義
+         * v_x := 並行速度x成分
+         * v_y := 並行速度y成分
+         * v_theta := 中心からタイヤまでの距離 * 角速度
+         */
+
+        void begin(float v_x = 0, float v_y = 0, float v_theta = 0) noexcept;
+
+    private:
+        void _setPowers_raw(float v1, float v2, float v3) noexcept;
+
+        void _setPowers_normalized(float v1, float v2, float v3) noexcept;
+
+    public:
+        void setPowers(float v1, float v2, float v3) noexcept;
+        void twist(float v_x, float v_y, float v_theta) noexcept;
+        inline void parallel(float v_x, float v_y) noexcept;
+        inline void rotate(float v_theta) noexcept;
+    }; // class LowerBody
+} // namespace ssr2
---
+++ src/ssr2/LowerBody.cpp
@@ -0,0 +1,65 @@
+#include "ssr2/LowerBody.hpp"
+
+ssr2::LowerBody::LowerBody(
+    uint8_t dir1, uint8_t pwm1,
+    uint8_t dir2, uint8_t pwm2,
+    uint8_t dir3, uint8_t pwm3,
+    uint16_t movAveCapacity
+) : _dir1(cos(PI / 2), sin(PI / 2)), _dir2(cos(PI * 7 / 6), sin(PI * 7 / 6)), _dir3(cos(PI * 11 / 6), sin(PI * 11 / 6)),
+    _motor1(dir1, pwm1), _motor2(dir2, pwm2), _motor3(dir3, pwm3),
+    _moving_average1(movAveCapacity), _moving_average2(movAveCapacity), _moving_average3(movAveCapacity)
+{}
+
+void ssr2::LowerBody::begin(float v_x, float v_y, float v_theta) {
+    _motor1.begin();
+    _motor2.begin();
+    _motor3.begin();
+    twist(v_x, v_y, v_theta);
+}
+
+void ssr2::LowerBody::_setPowers_raw(float v1, float v2, float v3) {
+    _moving_average1.write(v1);
+    _moving_average2.write(v2);
+    _moving_average3.write(v3);
+    int16_t p1 = static_cast<int16_t>(_moving_average1.read());
+    int16_t p2 = static_cast<int16_t>(_moving_average2.read());
+    int16_t p3 = static_cast<int16_t>(_moving_average3.read());
+    _motor1.write(p1);
+    _motor2.write(p2);
+    _motor3.write(p3);
+    #ifdef SSR_VERBOSE
+    char buffer[256] = "";
+    snprintf_P(buffer, 255, PSTR("[ssr2::LowerBody] set motor powers as %4d, %4d, %4d\n"), p1, p2, p3);
+    Serial.print(buffer);
+    #endif /* SSR_VERBOSE */
+}
+
+void ssr2::LowerBody::_setPowers_normalized(float v1, float v2, float v3) {
+    float max_v = max(max(abs(v1), abs(v2)), abs(v3));
+    float scale = 255.0 / max_v;
+    _setPowers_raw(v1 * scale, v2 * scale, v3 * scale);
+}
+
+void ssr2::LowerBody::setPowers(float v1, float v2, float v3) {
+    float max_v = max(max(abs(v1), abs(v2)), abs(v3));
+    if (max_v > 255.0) {
+        _setPowers_normalized(v1, v2, v3);
+    } else {
+        _setPowers_raw(v1, v2, v3);
+    }
+}
+
+void ssr2::LowerBody::twist(float v_x, float v_y, float v_theta) {
+    float v1 = _dir1.dot(v_x, v_y) + v_theta;
+    float v2 = _dir2.dot(v_x, v_y) + v_theta;
+    float v3 = _dir3.dot(v_x, v_y) + v_theta;
+    setPowers(v1, v2, v3);
+}
+
+void ssr2::LowerBody::parallel(float v_x, float v_y) {
+    twist(v_x, v_y, 0.0);
+}
+
+void ssr2::LowerBody::rotate(float v_theta) {
+    twist(0.0, 0.0, v_theta);
+}
